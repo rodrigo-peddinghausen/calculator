dfccc3a485ffcd605d066ed248e4e18ce7ba6dfa
Rodrigo Peddinghausen <rodpedd@gmail.com>
2021-01-25 01:36:32 -0300

    calculadora 2.0 - com uns floreis de JQuery :art:

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 let valorDisplay = '';
 
+let count = 0;
+
+let contas = [];
+let resultados = [];
+
+alert(`Basta abrir, fazer algumas contas e clicar para fechar! :D`)
+
+
+$('.entra').click(function(){
+    $('.master').show();
+    $('#btnFecha').show();
+    $('.entra').hide();
+})
+
+
 $('.botaoNum').click(function(){
-    $('.calcDisplay').val($('.calcDisplay').val() + $(this).val());
+    if(count==0){
+        $('.calcDisplay').val($('.calcDisplay').val() + $(this).val());
+    }
+    else{
+        $('.calcDisplay').val('');
+        $('.calcDisplay').val($('.calcDisplay').val() + $(this).val());
+        valorDisplay = '';
+        count = 0;
+    }
 });
 
 $('.botaoClear').click(function(){
@@ -15,11 +38,38 @@ $('.botaoOp').click(function(){
 });
 
 $('.botaoRes').click(function() {
-    let valorFinal = valorDisplay + $('.calcDisplay').val();
-    $('.calcDisplay').val('');
-    $('.calcDisplay').val(eval(valorFinal));
+    if(count == 0){
+        let valorFinal = valorDisplay + $('.calcDisplay').val();
+        $('.calcDisplay').val('');
+        $('.calcDisplay').val(eval(valorFinal));
+        contas.push(valorFinal);
+        resultados.push(eval(valorFinal));
+        count++;
+    }
 });
 
 $('.btnComma').click(function(){
     $('.calcDisplay').val($('.calcDisplay').val() + '.');
-})
+});
+
+$('#btnFecha').click(function(){
+    $('table').hide();
+    $('input').hide();
+    $('.master').animate({
+        'width': 500,
+        'height': 500
+    },
+    {
+        duration: 1000,
+        complete:function(){
+            $('.master').append('<h1>Contas<h1>');
+            $('.master').append('<ul id="lista"></ul>');
+            resultados.forEach(adicionaLi);
+        }
+    });
+});
+
+
+function adicionaLi(item, index, arr){
+    $('#lista').append(`<li> ${contas[index]} = ${resultados[index]}</li>`);
+}
